b7b2fe1163d9a01e2781aa108bd1e3c014b2d479
Marcin <marcinegzmont@gmail.com>
2023-09-10 07:22:40 +0100

    little changes

--- src/assets/projects/data/data.js
+++ src/assets/projects/data/data.js
@@ -12,7 +12,7 @@ export const data = [
     description: "Inventory Management App",
     technology: "Python Flask MySQL",
     moreInfo: `This CRUD application tracks inventory for a DVD shop.
-    It allows the shop owner to manage their inventory, by adding, updating and removing items. Additionally, the app tracks low stock levels and alerts the owner when inventory is running low. It can also calculate profit per item, allowing the owner to make informed decisions about inventory management.`,
+    It allows the shop owner to manage their inventory, by adding, updating and removing items. Additionally, the app tracks low stock levels and alerts the owner when inventory is running low. It can also calculate profit per item.`,
   },
   {
     id: 2,
--- src/components/VideoModal.jsx
+++ src/components/VideoModal.jsx
@@ -5,7 +5,7 @@ function VideoModal({ videoUrl, onClose }) {
   return (
     <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-60">
       <div className="relative p-4 bg-white rounded-lg shadow-md w-160 md:w-192">
-        <video controls autoPlay>
+        <video controls autoPlay muted>
           <source src={videoUrl} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
